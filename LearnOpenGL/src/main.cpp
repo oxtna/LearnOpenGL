9edaa9ed7b727c210258ae6da655c3e9fd06a770
oxtna <rafalkajomof2709@gmail.com>
2024-12-27 18:14:37 +0100

    Add element buffer and indexed drawing.

--- LearnOpenGL/src/main.cpp
+++ LearnOpenGL/src/main.cpp
@@ -107,16 +107,21 @@ int main()
          0.5f, -0.5f,  0.0f,
          0.0f,  0.5f,  0.0f,
     };
+    const unsigned int indices[] = {
+        0, 1, 2,
+    };
     // clang-format on
 
-    GLuint VAO;
+    GLuint VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
-    GLuint VBO;
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(0));
     glEnableVertexAttribArray(0);
@@ -130,7 +135,7 @@ int main()
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -138,6 +143,7 @@ int main()
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
     glfwTerminate();
