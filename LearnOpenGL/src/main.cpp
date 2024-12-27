6f74d93da8a90cf027bd4d30320625233b9d2cc3
oxtna <rafalkajomof2709@gmail.com>
2024-12-27 02:19:02 +0100

    Add vertex and fragment shaders, and render first triangle.

--- LearnOpenGL/src/main.cpp
+++ LearnOpenGL/src/main.cpp
@@ -1,6 +1,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 const unsigned int DEFAULT_WIDTH = 800;
 const unsigned int DEFAULT_HEIGHT = 600;
@@ -10,52 +13,146 @@ void processInput(GLFWwindow* window);
 
 int main()
 {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(DEFAULT_WIDTH, DEFAULT_HEIGHT, "LearnOpenGL", NULL, NULL);
-	if (window == NULL)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+    GLFWwindow* window = glfwCreateWindow(DEFAULT_WIDTH, DEFAULT_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
 
-	while (!glfwWindowShouldClose(window))
-	{
-		processInput(window);
+    GLuint shaderProgram;
+    {
+        GLint success;
+        const GLsizei INFO_LOG_SIZE = 512;
+        GLchar infoLog[INFO_LOG_SIZE]{};
 
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+        GLuint vertexShader;
+        {
+            std::ifstream shaderFile("main.vert");
+            std::ostringstream buffer;
+            buffer << shaderFile.rdbuf();
 
-		glfwPollEvents();
-		glfwSwapBuffers(window);
-	}
+            const std::string& shaderSource = buffer.str();
+            const GLchar* const shaders[] = {shaderSource.c_str()};
+            const GLint lengths[] = {static_cast<GLint>(shaderSource.length())};
 
-	glfwTerminate();
-	return 0;
+            vertexShader = glCreateShader(GL_VERTEX_SHADER);
+            glShaderSource(vertexShader, 1, shaders, lengths);
+            glCompileShader(vertexShader);
+
+            GLint success;
+            GLchar infoLog[512]{};
+            glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+            if (!success)
+            {
+                glGetShaderInfoLog(vertexShader, sizeof(infoLog) / sizeof(GLchar), NULL, infoLog);
+                std::cerr << "Error: Vertex shader compilation failed\n" << infoLog << std::endl;
+            }
+        }
+        GLuint fragmentShader;
+        {
+            std::ifstream shaderFile("main.frag");
+            std::ostringstream buffer;
+            buffer << shaderFile.rdbuf();
+
+            const std::string& shaderSource = buffer.str();
+            const GLchar* const shaders[] = {shaderSource.c_str()};
+            const GLint lengths[] = {static_cast<GLint>(shaderSource.length())};
+
+            fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+            glShaderSource(fragmentShader, 1, shaders, lengths);
+            glCompileShader(fragmentShader);
+
+            glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+            if (!success)
+            {
+                glGetShaderInfoLog(fragmentShader, INFO_LOG_SIZE, NULL, infoLog);
+                std::cerr << "Error: Fragment shader compilation failed\n" << infoLog << std::endl;
+            }
+        }
+        shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glLinkProgram(shaderProgram);
+
+        glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+        if (!success)
+        {
+            glGetProgramInfoLog(shaderProgram, INFO_LOG_SIZE, NULL, infoLog);
+            std::cerr << "Error: Program linking failed\n" << infoLog << std::endl;
+        }
+
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+    }
+
+    // clang-format off
+    const float vertices[] = {
+        -0.5f, -0.5f,  0.0f,
+         0.5f, -0.5f,  0.0f,
+         0.0f,  0.5f,  0.0f,
+    };
+    // clang-format on
+
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(0));
+    glEnableVertexAttribArray(0);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
+    glfwTerminate();
+    return 0;
 }
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height)
 {
-	glViewport(0, 0, width, height);
+    glViewport(0, 0, width, height);
 }
 
 void processInput(GLFWwindow* window)
 {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-	{
-		glfwSetWindowShouldClose(window, 1);
-	}
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, 1);
+    }
 }
